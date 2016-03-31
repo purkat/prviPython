5b6ace5347ac993193c2995a75034dd07afd5fea
Urban Purkat <urban.purkat@gmail.com>
2016-03-31 17:19:07 +0200

    Operacije

---
+++ numbers.py
@@ -0,0 +1,21 @@
+x = 5
+y = 30
+
+# sestevanje
+print x + y
+
+# mnozenje
+print x * y
+
+# deljenje
+print y / x
+
+# odstevanje
+print y - x
+
+print "==============="
+
+# deljenje
+print x / 2.0
+print float(x) / 2
+
