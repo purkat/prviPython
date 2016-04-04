2ad0e862982869639a0fd978185ce11d885d5c53
Urban Purkat <urban.purkat@gmail.com>
2016-04-04 18:03:53 +0200

    List

--- seznami.py
+++ seznami.py
@@ -1,17 +1,44 @@
-cars = ["renault", "audi", "bmw", "peugeot", "seat"]
-print cars
-print cars.index("bmw")
-print len(cars)
-cars.remove("audi")
-print cars
-print len(cars)
+'''
+items = ["milk", "bread", "eggs", "juice", "salami", "joghurt"]
 
-cars1 = ["renault", "audi", "bmw", "audi", "peugeot", "seat"]
-print cars1.count("audi")
-cars1.remove("audi")
-print cars1.count("audi")
+prices = [2.25, 1.68, 3.15, 1.22, 1.43, 2.87]
 
-if "audi" in cars1:
-    print("Audi is in the cars list")
+chosen_item = raw_input("What item would you like to buy? ")
+
+if chosen_item in items:
+    item_index = items.index(chosen_item)
+    item_price = prices[item_index]
+    print "Item price is %s" % (item_price)
 else:
-    print("Audi is NOT in the cars list")
+    print "Item is not available."
+'''
+items_prices = {
+    "milk": 2.25,
+    "bread": 1.68,
+    "eggs": 3.15,
+    "juice": 1.22,
+    "salami": 1.43,
+    "joghurt": 2.87
+}
+
+print items_prices.keys()
+print items_prices.values()
+
+chosen_item = raw_input("Izberi izdelek: ")
+
+if chosen_item in items_prices.keys():
+    price = items_prices[chosen_item]
+    print "Item price is %s" % (price)
+else:
+    print "Item is not available."
+cars = {
+    "clio": ["renault", ["red", "blue"]],
+    "golf": {"znamka": "volkswagen", "barve": ["red", "blue"], "vrata": 4},
+    "laguna": "renault",
+    "m3": "bmw",
+    "cordoba": "seat"
+}
+print cars["golf"]["barve"][1]
+
+
+
