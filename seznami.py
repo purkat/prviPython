e90636686475c50b36dec81abb9d96fcc764d70b
Urban Purkat <urban.purkat@gmail.com>
2016-04-04 17:33:49 +0200

    Liste

---
+++ seznami.py
@@ -0,0 +1,12 @@
+new_list = []
+print new_list
+new_list.append("first element")
+print new_list
+new_list.append("new element")
+print new_list
+my_var = "some variable"
+new_list.append(my_var)
+print new_list
+my_var = "aaaa"
+new_list.append(my_var)
+print new_list
